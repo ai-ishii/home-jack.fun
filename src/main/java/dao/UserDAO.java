e548e5b91480bc90c3847c6662dcb8cb02e65789
dj-urabe <dj-urabe@LAPTOP-1GBEVK5S>
2025-07-08 16:44:00 +0900

    ユーザー一覧Servlet完成

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -12,7 +12,6 @@ import bean.User;
 public class UserDAO {
 
 	//接続用の情報をフィールドに定数として定義
-<<<<<<< HEAD
 	private static final String RDB_DRIVE = "org.mariadb.jdbc.Driver";
 	private static final String URL = "jdbc:mariadb://localhost/jackdb";
 	private static final String USER = "root";
@@ -55,28 +54,28 @@ public class UserDAO {
 
 			while (rs.next()) {
 				User user = new User();
-				user.setUser_id(rs.getInt("user_id"));
-				user.setAcount_id(rs.getInt("account_id"));
+				user.setUserId(rs.getInt("user_id"));
+				user.setAcountId(rs.getInt("account_id"));
 				user.setName(rs.getString("name"));
-				user.setName_kana(rs.getString("name_kana"));
+				user.setNameKana(rs.getString("name_kana"));
 				user.setBirthday(rs.getDate("birthday"));
 				user.setAddress(rs.getString("address"));
 				user.setPhone(rs.getString("phone"));
-				user.setNearest_station(rs.getString("nearest_station"));
+				user.setNearestStation(rs.getString("nearest_station"));
 				user.setTransportation(rs.getString("transportation"));
 				user.setSex(rs.getString("sex"));
-				user.setEmployee_number(rs.getString("employee_number"));
+				user.setEmployeeNumber(rs.getString("employee_number"));
 				user.setDepartment(rs.getString("department"));
 				user.setTeam(rs.getString("team"));
-				user.setJoining_date(rs.getTimestamp("joining_date"));
-				user.setWork_history(rs.getInt("work_history"));
-				user.setMarriage_flag(rs.getInt("marriage_flag"));
+				user.setJoiningDate(rs.getTimestamp("joining_date"));
+				user.setWorkHistory(rs.getInt("work_history"));
+				user.setMarriageFlag(rs.getInt("marriage_flag"));
 				user.setChildren(rs.getInt("children"));
 				user.setQualification(rs.getString("qualification"));
-				user.setDisplay_flag(rs.getInt("display_flag"));
-				user.setRest_flag(rs.getInt("rest_flag"));
-				user.setRegist_date(rs.getTimestamp("regist_date"));
-				user.setUpdate_date(rs.getTimestamp("update_date"));
+				user.setDisplayFlag(rs.getInt("display_flag"));
+				user.setRestFlag(rs.getInt("rest_flag"));
+				user.setRegistDate(rs.getTimestamp("regist_date"));
+				user.setUpdateDate(rs.getTimestamp("update_date"));
 				userList.add(user);
 			}
 		} catch (Exception e) {
@@ -98,25 +97,4 @@ public class UserDAO {
 		return userList;
 	}
 
-=======
-		private static final String RDB_DRIVE="org.mariadb.jdbc.Driver";
-	 	private static final String URL="jdbc:mariadb://localhost/jackdb";
-	 	private static final String USER="root";
-	 	private static final String PASSWD="root123";
-	 
-	 	/**
-	 	 * データベース接続を行うメソッド
-	 	 * データベース接続用定義を基にデータベースへ接続し、戻り値としてコネクション情報を返す
-	 	 * @return con
-	 	 */
-	 	private static Connection getConnection(){
-	 		try{
-	 			Class.forName(RDB_DRIVE);
-	 			Connection con = DriverManager.getConnection(URL, USER, PASSWD);
-	 			return con;
-	 		}catch(Exception e){
-	 			throw new IllegalStateException(e);
-	 		}
-	 	}
->>>>>>> branch 'master' of https://github.com/ai-ishii/home-jack.fun.git
 }
