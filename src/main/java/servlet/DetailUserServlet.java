ba49c7d29c74d5e0e097e093d4c2d4672e9c8dbd
dj-urabe <dj-urabe@LAPTOP-1GBEVK5S>
2025-07-08 16:17:02 +0900

    作りかけでございます

---
+++ src/main/java/servlet/DetailUserServlet.java
@@ -0,0 +1,23 @@
+/*
+ * プログラム名：Home-Jack.ver.2.0
+ * 作成者：占部虎司郎
+ * 作成日：2025/6/18
+ */
+
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import bean.User;
+import dao.UserDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/detailUser")
+public class DetailUserServlet extends HttpServlet {
+
+}
