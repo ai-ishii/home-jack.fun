bfb687a4cfcc6dac7402b701ba0ec163ddbaca32
dj-okita <dj-okita@LAPTOP-SN9A2ER7>
2025-07-10 17:21:49 +0900

    お知らせ周辺機能です

--- src/main/java/servlet/AnnounceDetailServlet.java
+++ src/main/java/servlet/AnnounceDetailServlet.java
@@ -11,35 +11,35 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/announce")
-public class AnnounceServlet extends HttpServlet {
-
-	public void doGet(HttpServletRequest request,
+@WebServlet("/detailAnnounce")
+public class AnnounceDetailServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest request, 
 			HttpServletResponse response)
 			throws ServletException, IOException {
-
+		
 		String error = "";
 		String cmd = "";
-
+		
 		ArrayList<Announce> announceList = new ArrayList<Announce>();
-
+		
+		// int aoounceId = Integer.parseInt(request.getParameter("announceId"));
+		
 		try {
 			AnnounceDAO announceDAO = new AnnounceDAO();
-
-			announceList = announceDAO.selectAnnounceAll();
+			
+			// announceDAO.selectByAnnounceId();
 		} catch (Exception e) {
-			cmd = "";
-			error = "予期せぬエラーが発生しました。" + e;
+			
 		} finally {
 			if (error != "") {
 				request.setAttribute("cmd", cmd);
 				request.setAttribute("error", error);
-				request.getRequestDispatcher("").forward(request, response);
+				request.getRequestDispatcher("#").forward(request, response);
 			} else {
-				request.setAttribute("announceList", announceList);
-				request.getRequestDispatcher("/view/announce.jsp").forward(request, response);
+				// request.setAttribute("announceList", announceList);
+				request.getRequestDispatcher("/view/announceDetail.jsp").forward(request, response);
 			}
 		}
 	}
-
 }
