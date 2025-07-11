13954128d3284dca2122d0b5775220180cd284eb
dj-aoki <dj-aoki@LAPTOP-E0VQHSON>
2025-07-11 09:39:14 +0900

    JackWorks関連更新

--- src/main/java/dao/JackWorksDAO.java
+++ src/main/java/dao/JackWorksDAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import bean.Jackworks;
@@ -97,70 +98,146 @@ public class JackWorksDAO {
 		}
 		return jackList;
 	}
+
+	/**
+	 * 引数のjackworksIdでJackWorksをDBから削除するメソッド
+	 * 
+	 * @return 
+	 */
+	public void delete(int jackworksId) {
+		Connection con = null;
+		Statement smt = null;
+
+		try {
+			con = getConnection();
+			smt = con.createStatement();
+
+			String sql = "DELETE FROM jackworks_info WHERE jackworks_id=" + jackworksId;
+
+			smt.executeUpdate(sql);
+
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		} finally {
+			if (smt != null) {
+				try {
+					smt.close();
+				} catch (SQLException ignore) {
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ignore) {
+				}
+			}
+		}
+	}
 	
+	/**
+	 * 引数のjackでJackWorks情報をDBに登録するメソッド
+	 * 
+	 * @return 
+	 */
+	public void insert(Jackworks jack) {
+		Connection con = null;
+		Statement smt = null;
+
+		try {
+			con = getConnection();
+			smt = con.createStatement();
+			
+			LocalDateTime nowDate = LocalDateTime.now();
+
+			String sql = "INSERT INTO jackworks_info VALUES (NULL, NULL, '"+jack.getName()+"', '"+jack.getEmployeeNumber()+"', '"+jack.getCategory()+"', "
+					+ "'"+jack.getAssessment()+"', '"+jack.getPoint()+"', '"+ nowDate +"', '"+jack.getNote()+"',"
+					+ " '"+jack.getProject()+"', '"+jack.getWorkSeason()+"', '"+jack.getPrice()+"', '"+jack.getPay()+"', '"+jack.getWorkPlace()+"', "
+					+ "'"+jack.getWorkContent()+"', '"+jack.getPhase()+"', '"+jack.getLanguage()+"', '"+jack.getSkill()+"', '"+jack.getNeedPeople()+"', "
+					+ "'"+jack.getSeller()+"', '"+jack.getContact()+"', '"+jack.getOther()+"', '"+jack.getManagerFlag()+"', '"+jack.getAdminFlag()+"')";
+
+			smt.executeUpdate(sql);
+
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		} finally {
+			if (smt != null) {
+				try {
+					smt.close();
+				} catch (SQLException ignore) {
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException ignore) {
+				}
+			}
+		}
+	}
+
 	/**
 	 * 引数のjackworks_idからDBのJackWorks情報を検索するメソッド
 	 * 戻り値としてJackWorks全情報を返す
 	 * @return jackList
 	 */
-	
-//	public Jackworks selectByJackWorksId(int jackworks_id) {
-//		
-//		Connection con = null;
-//		Statement smt = null;
-//
-//		Jackworks jack=new Jackworks();
-//
-//		try {
-//			con = getConnection();
-//			smt = con.createStatement();
-//
-//			String sql = "SELECT * FROM jackworks_info WHERE jackworks_id="+jackworks_id;
-//			ResultSet rs = smt.executeQuery(sql);
-//
-//			while (rs.next()) {
-//				jack.setJackworksId(rs.getInt("jackworks_id"));
-//				jack.setUserId(rs.getInt("user_id"));
-//				jack.setEmployeeNumber(rs.getInt("employee_number"));
-//				jack.setName(rs.getString("name"));
-//				jack.setCategory(rs.getString("category"));
-//				jack.setAssessment(rs.getString("assessment"));
-//				jack.setPoint(rs.getInt("point"));
-//				jack.setPointsGetDate(rs.getTimestamp("points_get_date"));
-//				jack.setNote(rs.getString("note"));
-//				jack.setProject(rs.getString("project"));
-//				jack.setWorkSeason(rs.getString("work_season"));
-//				jack.setPrice(rs.getInt("price"));
-//				jack.setPay(rs.getInt("pay"));
-//				jack.setWorkPlace(rs.getString("work_place"));
-//				jack.setWorkContent(rs.getString("work_content"));
-//				jack.setPhase(rs.getString("phase"));
-//				jack.setLanguage(rs.getString("language"));
-//				jack.setSkill(rs.getString("skill"));
-//				jack.setNeedPeople(rs.getInt("need_people"));
-//				jack.setSeller(rs.getString("seller"));
-//				jack.setContact(rs.getString("contact"));
-//				jack.setOther(rs.getString("other"));
-//				jack.setManagerFlag(rs.getInt("manager_flag"));
-//				jack.setAdminFlag(rs.getInt("admin_flag"));
-//			}
-//
-//		} catch (Exception e) {
-//			throw new IllegalStateException(e);
-//		} finally {
-//			if (smt != null) {
-//				try {
-//					smt.close();
-//				} catch (SQLException ignore) {
-//				}
-//			}
-//			if (con != null) {
-//				try {
-//					con.close();
-//				} catch (SQLException ignore) {
-//				}
-//			}
-//		}
-//		return jack;
-//	}
+
+	//	public Jackworks selectByJackWorksId(int jackworks_id) {
+	//		
+	//		Connection con = null;
+	//		Statement smt = null;
+	//
+	//		Jackworks jack=new Jackworks();
+	//
+	//		try {
+	//			con = getConnection();
+	//			smt = con.createStatement();
+	//
+	//			String sql = "SELECT * FROM jackworks_info WHERE jackworks_id="+jackworks_id;
+	//			ResultSet rs = smt.executeQuery(sql);
+	//
+	//			while (rs.next()) {
+	//				jack.setJackworksId(rs.getInt("jackworks_id"));
+	//				jack.setUserId(rs.getInt("user_id"));
+	//				jack.setEmployeeNumber(rs.getInt("employee_number"));
+	//				jack.setName(rs.getString("name"));
+	//				jack.setCategory(rs.getString("category"));
+	//				jack.setAssessment(rs.getString("assessment"));
+	//				jack.setPoint(rs.getInt("point"));
+	//				jack.setPointsGetDate(rs.getTimestamp("points_get_date"));
+	//				jack.setNote(rs.getString("note"));
+	//				jack.setProject(rs.getString("project"));
+	//				jack.setWorkSeason(rs.getString("work_season"));
+	//				jack.setPrice(rs.getInt("price"));
+	//				jack.setPay(rs.getInt("pay"));
+	//				jack.setWorkPlace(rs.getString("work_place"));
+	//				jack.setWorkContent(rs.getString("work_content"));
+	//				jack.setPhase(rs.getString("phase"));
+	//				jack.setLanguage(rs.getString("language"));
+	//				jack.setSkill(rs.getString("skill"));
+	//				jack.setNeedPeople(rs.getInt("need_people"));
+	//				jack.setSeller(rs.getString("seller"));
+	//				jack.setContact(rs.getString("contact"));
+	//				jack.setOther(rs.getString("other"));
+	//				jack.setManagerFlag(rs.getInt("manager_flag"));
+	//				jack.setAdminFlag(rs.getInt("admin_flag"));
+	//			}
+	//
+	//		} catch (Exception e) {
+	//			throw new IllegalStateException(e);
+	//		} finally {
+	//			if (smt != null) {
+	//				try {
+	//					smt.close();
+	//				} catch (SQLException ignore) {
+	//				}
+	//			}
+	//			if (con != null) {
+	//				try {
+	//					con.close();
+	//				} catch (SQLException ignore) {
+	//				}
+	//			}
+	//		}
+	//		return jack;
+	//	}
 }
