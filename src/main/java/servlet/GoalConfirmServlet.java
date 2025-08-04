e89accde0a35584ce329e60dc53bffbcf651ed50
dj-gekko <dj-gekko@DESKTOP-PF6IS38>
2025-08-04 09:12:34 +0900

    個人目標系のサーブレット、JSPの修正を行いました。

--- src/main/java/servlet/GoalConfirmServlet.java
+++ src/main/java/servlet/GoalConfirmServlet.java
@@ -24,6 +24,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 
 @WebServlet("/goalConfirm")
@@ -46,6 +47,7 @@ public class GoalConfirmServlet extends HttpServlet {
 		String error = "";
 		String strUserId = "";
 		String cmd = "";
+		Integer userId = 0;
 
 		//オブジェクト宣言
 		Goal goal = new Goal();
@@ -59,21 +61,24 @@ public class GoalConfirmServlet extends HttpServlet {
 		//配列宣言
 		ArrayList<QuarterGoal> quarterGoalList = new ArrayList<QuarterGoal>();
 
+		//セッションオブジェクトの生成
+		HttpSession session = request.getSession();
+
+
 		try {
 			//getParameterメソッドを使い、取得した値を代入する
-			strUserId = request.getParameter("user_id");
+		//	userId = (Integer)session.getAttribute("user_id");
 			cmd = request.getParameter("cmd");
 
-			//セッション登録がまだなため、仮で登録
+			//セッション登録がまだなため、仮で登録してます
 			strUserId = "4";
-			
 			//※ここは後で絶対に変える文章なので覚えておいてください
 			if (cmd == null) {
 				cmd = ""; 
 			}
 			
 			//userIdをStringからintへキャスト
-			int userId = Integer.parseInt(strUserId);
+			userId = Integer.parseInt(strUserId);
 
 			//部目標を呼び出す
 			teamGoal = teamGoalDAO.selectByUserId(userId);
@@ -92,6 +97,7 @@ public class GoalConfirmServlet extends HttpServlet {
 				request.getRequestDispatcher("/view/error.jsp").forward(request, response);
 			}
 			//リクエストスコープを使ってフォワード
+		//	session.setAttribute("userId",userId);
 			request.setAttribute("teamGoal", teamGoal);
 			request.setAttribute("goal", goal);
 			request.setAttribute("quarter_goal_list", quarterGoalList);
