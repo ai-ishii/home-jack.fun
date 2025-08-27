f2e4fe5ba4d8eeaaabd0c437d96a982a66f738a3
dj-urabe <dj-urabe@LAPTOP-1GBEVK5S>
2025-08-27 13:49:22 +0900

    失われたものを拾った

--- src/main/java/filter/AuthFilter.java
+++ src/main/java/filter/AuthFilter.java
@@ -4,12 +4,15 @@
  * 作成者：月向亮太
  * 
  * 作成日：2025/8/25
- * 最終更新日: 2025/8/25
+ * 最終更新日: 2025/8/27
  */
 package filter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import bean.AuthorityHaving;
+import dao.AuthorityDAO;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -22,23 +25,57 @@ import jakarta.servlet.http.HttpSession;
 
 @WebFilter({"/momthJackworks" })
 public class AuthFilter implements Filter {
-
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 
+		//オブジェクト宣言
+		AuthorityDAO autorityDAO = new AuthorityDAO();
+		ArrayList<AuthorityHaving> authorityList = new ArrayList<AuthorityHaving>();
+		//権限格納用
+		String userRole = "";
+		//権限があればtrue、なければfalse
+		boolean flag = false;
+
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		//現在のセッションを取得
+		//false=セッションが存在しない場合に新しく作成しない
 		HttpSession session = httpRequest.getSession(false);
+		String cmd = (String) request.getAttribute("cmd");
+
+		//セッションからユーザーIDを受け取る
+		Integer userId = (Integer) session.getAttribute("user_id");
+		//ユーザーIDから権限リストを取得するメソッド
+		authorityList = autorityDAO.selectByUserId(userId);
+
 		
-		String userRole = (session!= null) ? (String) session.getAttribute("userRole") : null;
-		
-		if (userRole != null && (userRole.equals("ADMIN") || userRole.equals("MANAGER"))) {
-	         chain.doFilter(request, response);
-	         
-	     } else {
-	         request.setAttribute("error", "このページにアクセスする権限がありません。");
-	         request.getRequestDispatcher("/view/error.jsp").forward(request, response);
-	     }
-		
+		if (authorityList == null) {
+
+		} else {
+			for (int i = 0; i < authorityList.size(); i++) {
+				AuthorityHaving authorityHaving = authorityList.get(i);
+				userRole = authorityHaving.getAuthorityCode();
+
+				//JackWorks承認者
+				if ((userRole.equals("APR_JACK"))) {
+					flag = true;
+
+					//JackWorks承認者とマネージャー
+				} else if ((userRole.equals("APR_JACK") || userRole.equals("POS_MNGR"))) {
+					flag = true;
+				}
+			}
+		}
+
+		if (false) {
+			request.setAttribute("cmd", cmd);
+			request.setAttribute("error", "このページにアクセスする権限がありません。");
+			request.getRequestDispatcher("/view/error.jsp").forward(request, response);
+		}
+
+		if (true) {
+			//アクセス許可(本来のServletへ遷移)
+			chain.doFilter(request, response);
+		}
 	}
 }
